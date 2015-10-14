462d91bdce18b2b2ac4bbb7c6b029ff8c4497932
MbL93 <Marcblickle@gmx.de>
2015-10-14 13:01:29 +0200

    Alles was ich bisher programmiert habe

---
+++ source/game.js
@@ -0,0 +1,87 @@
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
+var bot;
+var ball;
+var ballreleased;
+
+
+function preload() {
+    
+    game.load.image('bot', 'images/Unbenannt1.png');
+    game.load.image('leicht', 'images/leicht.png');
+    game.load.image('ball', 'images/ball.png');
+    game.load.image('stars','images/background1.png');
+}
+
+
+function create() {
+    
+    land = game.add.tileSprite(0, 0, 800, 600, 'stars');
+    //game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    bot = game.add.sprite(game.world.centerX, 550, 'bot');
+    bot.anchor.setTo(0.5, 0.5);
+    bot.scale.setTo(2, 2);
+   
+    ball = game.add.sprite(game.world.centerX-15, 505, 'ball');
+    game.physics.arcade.enable(ball);
+    
+    
+    bricks = game.add.group();
+    bricks.enableBody = true;
+   // bricks.physicsBodyType = Phaser.Physics.ARCADE;
+
+    var brick;
+
+        for (var y = 0; y < 4; y++)
+        {
+            for (var x = 0; x < 15; x++)
+            {
+                brick = bricks.create(120 + (x * 36), 100 + (y * 52), 'leicht', 'leicht.png');
+                brick.body.bounce.set(1);
+                brick.body.immovable = true;
+            }
+        }
+    
+
+    game.input.mouse.capture = true;
+    ballreleased=false;
+}
+   
+
+function update() {
+        
+      if (game.input.activePointer.leftButton.isDown)
+    {
+        ballreleased = true;
+         ball.body.velocity.y=-150;
+    }
+    else {}
+   
+
+  if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+    {
+        if(!ballreleased){ 
+        ball.x -= 4 ;}
+        bot.x -= 4;
+        
+        
+    }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+    {
+        
+        if(!ballreleased){ 
+        ball.x += 4 ;}
+        bot.x += 4;
+        
+    }
+    
+    
+  
+}
+ 
+
+
+        
+
+
+
