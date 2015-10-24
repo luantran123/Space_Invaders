5f389fcc650a4ccbe0d088306e5c5a3cd128cd3c
olivierMboussi <mbousol@yahoo.fr>
2015-10-24 12:31:56 +0200

    Physics on Mario Im MainMenü

--- source/mainmenu.js
+++ source/mainmenu.js
@@ -8,6 +8,10 @@
 
 Break.MainMenu.prototype = {
     create: function() {
+		this.physics.startSystem(Phaser.Physics.ARCADE);
+		//Activate Keyboard
+	cursors = this.input.keyboard.createCursorKeys();
+		
         background = this.add.tileSprite(0, 0, 1024, 768, 'mario0');
         startbutton= this.game.add.button(200, 430, 'startbutton', this.startGame, this);
         startbutton.anchor.setTo(0.5,0.5);
@@ -27,13 +31,13 @@ Break.MainMenu.prototype = {
         supermario.anchor.set(0.5);
 
         this.physics.arcade.enable(supermario);
-    	supermario.body.velocity.setTo(0, -700);
-        supermario.body.gravity.set(0, 7000);
+    	supermario.body.velocity.setTo(0, 0);
+        supermario.body.gravity.set(0, 600);
         supermario.body.collideWorldBounds = true;
         supermario.checkWorldBounds = true;
-        supermario.body.bounce.set(1);
-        supermario.body.immovable = true;
-        
+        supermario.body.bounce.set(0.21);
+        //supermario.body.immovable = true;
+    
         
         //Olivier: Das hier ist "Boden". Mario soll darauf herumhüpfen (so wie Cursor)
         boden= this.add.sprite(0, 705, 'boden');
@@ -44,11 +48,48 @@ Break.MainMenu.prototype = {
         boden.checkWorldBounds = true;
         boden.body.bounce.set(1);
             
+		this.physics.arcade.collide(supermario, boden);
 
         
     },
     
-    
+    update: function() {
+	
+	this.physics.arcade.collide(supermario, boden);
+	
+	//  Reset the velocity
+	supermario.body.velocity.x = 0;
+
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        supermario.body.velocity.x = -150;
+
+        supermario.animations.play('left');
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        supermario.body.velocity.x = 150;
+
+        supermario.animations.play('right');
+    }
+    else
+    {
+        //  Do nothing
+        supermario.animations.stop();
+
+        supermario.frame = 4;
+    }
+
+    //  Jump only if Mario is already touching the ground
+    if (cursors.up.isDown && supermario.body.touching.down)
+    {
+        supermario.body.velocity.y = -550;
+    }
+	
+	},
+	
     startGame: function() {
         this.game.state.start('Game');
     }
